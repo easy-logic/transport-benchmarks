cb4fa30286e8448407ac0dba8fd6e72920a6b628
QIvan <qtivan@gmail.com>
2020-08-08 02:11:22 +0300

    refactor dockerfiles

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./gradlew clean build
+
+docker-compose -f aeron-docker-compose.yml  up --abort-on-container-exit
+docker-compose -f aeron-docker-compose.yml  down
+docker-compose -f rabbitmq-docker-compose.yml  up --abort-on-container-exit
+docker-compose -f rabbitmq-docker-compose.yml  down
+docker-compose -f spring-boot-docker-compose.yml  up --abort-on-container-exit
+docker-compose -f spring-boot-docker-compose.yml  down
